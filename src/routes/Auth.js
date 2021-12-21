c62e85d97964ab3ab8b373acaf7e754845f79f90
MONO-ON <dmsgh112@gmail.com>
2021-12-22 03:19:22 +0900

    chapter 0~2 review required

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,7 +3,11 @@ import {
   createUserWithEmailAndPassword,
   getAuth,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider
 } from "@firebase/auth";
+import { authService, firebaseInstance } from "fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -23,8 +27,8 @@ const Auth = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
-        let data;
-        const auth = getAuth();
+      let data;
+      const auth = getAuth();
       if (newAccount) {
         data = await createUserWithEmailAndPassword(auth, email, password);
       } else {
@@ -35,8 +39,19 @@ const Auth = () => {
       setError(error.message);
     }
   };
-const toggleAccount = () => setNewAccount((prev) => !prev);
-
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const { target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    const data = await signInWithPopup(authService, provider);
+    console.log(data)
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -61,8 +76,8 @@ const toggleAccount = () => setNewAccount((prev) => !prev);
       </form>
       <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">Continue with Google</button>
+        <button onClick={onSocialClick} name="github">Continue with Github</button>
       </div>
     </div>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,15 @@
+import { authService } from "fbase";
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const onLogOutClick = () => {
+    authService.signOut();
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick} ><Link to="/">Log Out</Link></button>
+    </>
+  );
+};
