87694f5fbcaf7b7b7d21f690c2120ff06b699015
mono <mono@MONO-iMac.local>
2021-12-21 22:01:34 +0900

    Complete login

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
+
+export default function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, [])
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
